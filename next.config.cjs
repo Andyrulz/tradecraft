9ff7a8066520f2c843c4d97cd4b5667c52f33f55
Andrew Abishek <aabishek@microsoft.com>
2025-06-21 03:09:28 +0530

    Revamp - images issues

--- next.config.cjs
+++ next.config.cjs
@@ -4,7 +4,29 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+    domains: [
+      'images.barrons.com',
+      'static.foxbusiness.com',
+      'static.seekingalpha.com',
+      'images.marketwatch.com',
+      'cdn.fxempire.com',
+      's3-symbol-logo.tradingview.com',
+      'media.reuters.com',
+      'nypost.com',
+      'static.benzinga.com',
+      'api.producthunt.com',
+      'gravatar.com',
+      'twelve.tools',
+      'www.traderinsight.com',
+      'finviz.com',
+      'stocktwits.com',
+      'www.alphaquery.com',
+      'www.smallcapdaily.com',
+      // Add more as needed
+    ],
+  },
   webpack: (config, { isServer }) => {
     config.cache = false;
     return config;
